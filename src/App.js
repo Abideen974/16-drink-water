59ca0acbf2f7a0a7640b95fa40cfb200aba0b2c9
Abideen <Abideenali926@gmail.com>
2022-04-12 23:38:19 -0700

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Drink from './components/Drink'
+
+function App() {
+  return (
+    <>
+
+      <Drink />
+    </>
+      
+    
+  );
+}
+
+export default App;
